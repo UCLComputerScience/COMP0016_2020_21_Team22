19e8eb0cb2a677ccb112f2624c48187da296ba17
jasnoor7 <56026103+jasnoor7@users.noreply.github.com>
2021-01-23 15:54:12 +0000

    Add files via upload

---
+++ Assets/DataPlotter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataPlotter : MonoBehaviour {
+ 
+ // Name of the input file, no extension
+ public string inputfile;
+ 
+  
+ // List for holding data from CSV reader
+ private List<Dictionary<string, object>> pointList;
+ 
+ // Use this for initialization
+ void Start () {
+ 
+ // Set pointlist to results of function Reader with argument inputfile
+ pointList = CSVReader.Read(inputfile);
+ 
+ //Log to console
+ Debug.Log(pointList);
+ }
+  
+}
